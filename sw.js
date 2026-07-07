78ad5539e31ed0f942daa52bf47b2c439ddd6844
sin1234tw-oss <sin1234tw@gmail.com>
2026-07-07 17:33:03 +0800

    1

--- sw.js
+++ sw.js
@@ -1,15 +1,12 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('photo-search-v1').then((cache) => {
-      return cache.addAll(['./', './index.html', './manifest.json']);
-    })
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+const CACHE_NAME = 'v1';
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((response) => {
+        let responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return response;
+      });
     })
   );
 });
