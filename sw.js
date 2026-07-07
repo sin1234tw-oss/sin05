4a5821258a0a9f08217b4db74c9bbb96c545218e
sin1234tw-oss <sin1234tw@gmail.com>
2026-07-07 18:18:41 +0800

    1

--- sw.js
+++ sw.js
@@ -1,12 +1,5 @@
-const CACHE_NAME = 'v1';
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).then((response) => {
-        let responseClone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
-        return response;
-      });
-    })
-  );
+  event.respondWith(caches.match(event.request).then((response) => {
+    return response || fetch(event.request);
+  }));
 });
